3e5dd857d6eb57395d85531c6122def64ee63589
Jacqueline Remmel, Molly Stambaugh and Sofia Giordano <jacqueline@>
2019-09-24 09:36:11 -0700

    add start game functionality, and win condition

--- higher-lower-game.cs
+++ higher-lower-game.cs
@@ -5,7 +5,34 @@ class HigherLowerGame
 {
   static void Main()
   {
-    int x = 5;
-    Console.WriteLine(x);
+    Console.WriteLine("Pick a number between 1-100 for me to guess.");
+    int computerGuess = GuessMiddleNumber(1, 100);
+    Console.WriteLine("Is your number " + computerGuess + "? (Enter Y/N)");
+    string answer = Console.ReadLine();
+    if (answer.ToUpper() == "Y")
+    {
+      Console.WriteLine("I won! Your number is: " + computerGuess);
+      Console.WriteLine("Play Again? (Enter Y/N)");
+      string newGame = Console.ReadLine();
+      if (newGame.ToUpper() == "Y")
+      {
+        Main();
+      } 
+      else 
+      {
+        Console.WriteLine("Goodbye.");
+      }
+    } 
+    else if (answer.ToUpper() == "N")
+    {
+
+    }
+
+
+  }
+  static int GuessMiddleNumber(int min, int max)
+  {
+    int halfwayNum = ((max - min)/2)+ min;
+    return halfwayNum;
   }
 }
