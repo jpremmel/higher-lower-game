811702ced75d24e2a498440d7cd859473c5354e5
Jacqueline Remmel, Molly Stambaugh and Sofia Giordano <jacqueline@>
2019-09-24 11:22:00 -0700

    works

--- higher-lower-game.cs
+++ higher-lower-game.cs
@@ -1,38 +1,49 @@
 using System;
-using System.Collections.Generic;
-
-class HigherLowerGame
+public class Program
 {
-  static void Main()
+  public static void Main()
   {
     Console.WriteLine("Pick a number between 1-100 for me to guess.");
-    int computerGuess = GuessMiddleNumber(1, 100);
-    Console.WriteLine("Is your number " + computerGuess + "? (Enter Y/N)");
+    int min = 1;
+    int max = 100;
+    GuessMiddleNumber(min, max);
+  }
+  static int GuessMiddleNumber(int min, int max)
+  // GuessMiddleNumber is a method for calculating the users number. We added the branching statements here because we need to repeat this series of questions until the user answers yes
+  {
+    int halfwayNum = ((max - min)/2)+ min;
+    Console.WriteLine("Is your number " + halfwayNum + "? (Enter Y/N)");
+
     string answer = Console.ReadLine();
-    if (answer.ToUpper() == "Y")
+    if (answer.ToUpper() == "Y" || answer.ToUpper() == "YES")
     {
-      Console.WriteLine("I won! Your number is: " + computerGuess);
+      Console.WriteLine("I won! Your number is: " + halfwayNum);
       Console.WriteLine("Play Again? (Enter Y/N)");
       string newGame = Console.ReadLine();
       if (newGame.ToUpper() == "Y")
       {
         Main();
-      } 
-      else 
+      }
+      else
       {
         Console.WriteLine("Goodbye.");
       }
-    } 
-    else if (answer.ToUpper() == "N")
-    {
-
     }
-
-
-  }
-  static int GuessMiddleNumber(int min, int max)
-  {
-    int halfwayNum = ((max - min)/2)+ min;
+    else
+    {
+      Console.WriteLine("Is your number higher, or lower? (Enter H/L)");
+      string response = Console.ReadLine();
+      if (response.ToUpper() == "H" || response.ToUpper() == "HIGHER")
+      {
+        min = halfwayNum;
+        GuessMiddleNumber(min, max);
+      }
+      else if (response.ToUpper() == "L" || response.ToUpper() == "LOWER")
+      {
+        max = halfwayNum;
+        GuessMiddleNumber(min, max);
+      }
+    }
     return halfwayNum;
   }
 }
